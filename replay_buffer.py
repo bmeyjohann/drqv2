b513e1ffccb590a4407dd103a9958b525ef52fe5
Benjamin Meyjohann <benjamin.meyjohann@gmx.de>
2025-11-11 15:39:34 +0100

    Fix seeding error.

--- replay_buffer.py
+++ replay_buffer.py
@@ -165,7 +165,7 @@ class ReplayBuffer(IterableDataset):
 
 
 def _worker_init_fn(worker_id):
-    seed = np.random.get_state()[1][0] + worker_id
+    seed = int(np.random.get_state()[1][0]) + int(worker_id)
     np.random.seed(seed)
     random.seed(seed)
 
